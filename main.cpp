030da950455b50ce476e0747a53620b130cfed9a
pclank <31415965+pclank@users.noreply.github.com>
2020-08-13 17:57:24 +0300

    Initial Graph Building and Printing + Basic Index-Object Data Structure.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
+#include "LEDA/graph/graph.h"
+#include "LEDA/graph/ugraph.h"
+#include "LEDA/core/random_source.h"
+#include "LEDA/core/p_queue.h"
+#include "LEDA/graph/graph_alg.h"
+#include "LEDA/core/dynamic_trees.h"
+#include "LEDA/graph/graph_misc.h"
+#include "LEDA/system/timer.h"
+
+using namespace leda;
+
+struct indexobj                                 // Object Saved in Index Array
+{
+    node* edge_target;
+    node* closure_source;
+
+    unsigned int refcount;
+};
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    graph G;                                        // Initial Graph Building Section
+
+    int nn;
+
+    std::cout << "Input Initial Graph Vertex Number: ";
+    std::cin >> nn;
+
+    complete_graph(G, nn);
+
+    std::cout << "Built Initial Graph G with " << nn << "Vertices!\n\n";
+
+    forall_edges(e, G)                              // Print Initial Graph Edges
+    {
+        G.print_edge(e);
+        std::cout << "\n";
+    }
+    `
     return 0;
 }
